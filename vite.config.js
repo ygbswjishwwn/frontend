bc475c042278b77cdf92134c67f0576bba3d1c68
ygbswjishwwn <yutian001002@gmail.com>
2026-06-24 00:54:32 +0800

    add tailwind vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
